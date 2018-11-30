e73d99ff42afac71392d9b6604f946ed4b28f783
Kelly Hawkshaw <hawkshaw@chapman.edu>
2018-11-30 11:50:27 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#cars.sh
+#Kelly Hawkshaw
+
+while [ X -eq 1 ]
+do
+
+	echo"My Old Cars Inventory System"
+	echo"1. Enter a car"
+	echo"2. List of cars"
+	echo"Q. Quit";read car
+		case "$car" in
+			"1")
+			"2")
+			"Q")
