1381eeb0f2440bffe4b960bfa32a52858cc84e47
khawkshaw16 <44701165+khawkshaw16@users.noreply.github.com>
2018-12-07 00:10:16 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -1,15 +1,32 @@
 #! /bin/bash
 #cars.sh
 #Kelly Hawkshaw
-
-while [ X -eq 1 ]
+int i = 0;
+while [[ i==0 ]]; 
 do
-
-	echo"My Old Cars Inventory System"
-	echo"1. Enter a car"
-	echo"2. List of cars"
-	echo"Q. Quit";read car
-		case "$car" in
-			"1")
-			"2")
-			"Q")
+  echo "My Old Cars Inventory System";
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~";
+  echo "1. Enter a Car";
+  echo "2. List the cars";
+  echo "Q. Quit";
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~";
+  read option;
+  case "$option" in
+    "1" ) echo "Year?";
+          read a;
+          echo "Make?";
+          read b;
+          echo "Model?";
+          read c;
+          d="$a:$b:$c";
+          echo "$d" >> my_old_cars;
+      ;;
+    "2" ) sort my_old_cars;
+      ;;
+    "Q" ) echo "Goodbye. Thank you.";
+          break;
+      ;;
+    * ) echo "Wrong Input"
+      ;;
+  esac
+done
